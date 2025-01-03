e98dba0176730ec977c9cb10251f52a90d6d95ab
Abhinavv007 <abhi00007vani@gmail.com>
2025-01-03 14:32:56 +0530

    HEEHEHAHAHA

---
+++ script.ts
@@ -0,0 +1,11 @@
+
+function printKaroReBaba(myM: string, itniBaar: number = 100): void {
+    for (let i = 0; i < itniBaar; i++) {
+      console.log(myM);
+    }
+  }
+  
+
+  const myM: string = "Hello, Bro! , Let's play BGMI🙂";
+  printKaroReBaba(myM);
+  
